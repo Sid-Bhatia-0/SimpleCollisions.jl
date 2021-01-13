b92e6441777d2bbd1601769979bc79edca3718aa
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-01-13 16:32:47 +0530

    rename to is_colliding and update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,37 +5,37 @@ using Test
 @testset "PhysicsEngine2D.jl" begin
     @testset "Rect2D intersection" begin
         a = Rect2D(1, 2, 3, 4)
-        b = Rect2D(4, 6, 1, 2)
-        @test PE2D.is_intersecting(a, b) == true
+        b = Rect2D(3, 4, 5, 6)
+        @test PE2D.is_colliding(a, b) == true
 
-        a = Rect2D(4, 6, 1, 2)
-        b = Rect2D(1, 2, 3, 4)
-        @test PE2D.is_intersecting(a, b) == true
+        a = Rect2D(1, 2, 3, 4)
+        b = Rect2D(4, 6, 1, 2)
+        @test PE2D.is_colliding(a, b) == true
+
+        a = Rect2D(1, 2, 3, 4)
+        b = Rect2D(0, 0, 6, 6)
+        @test PE2D.is_colliding(a, b) == true
 
         a = Rect2D(1, 2, 3, 4)
         b = Rect2D(4, 2, 1, 2)
-        @test PE2D.is_intersecting(a, b) == true
+        @test PE2D.is_colliding(a, b) == true
 
         a = Rect2D(1, 2, 3, 4)
         b = Rect2D(5, 6, 7, 8)
-        @test PE2D.is_intersecting(a, b) == false
-
-        a = Rect2D(1, 2, 3, 4)
-        b = Rect2D(-2, -2, 1, 2)
-        @test PE2D.is_intersecting(a, b) == false
+        @test PE2D.is_colliding(a, b) == false
     end
 
     @testset "Circle intersection" begin
         a = Circle(Point2(0, 0), 1)
         b = Circle(Point2(0, 0), 1)
-        @test PE2D.is_intersecting(a, b) == true
+        @test PE2D.is_colliding(a, b) == true
 
         a = Circle(Point2(0, 0), 1)
         b = Circle(Point2(2, 0), 1)
-        @test PE2D.is_intersecting(a, b) == true
+        @test PE2D.is_colliding(a, b) == true
 
         a = Circle(Point2(0, 0), 1)
         b = Circle(Point2(3, 0), 1)
-        @test PE2D.is_intersecting(a, b) == false
+        @test PE2D.is_colliding(a, b) == false
     end
 end
