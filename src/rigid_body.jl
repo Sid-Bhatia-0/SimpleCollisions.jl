df3e6fd7f9e7baa8d62a13d047a67dc210a79de6
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-01-13 23:29:07 +0530

    add getters and setters (#5)
    * add package MacroTools
    
    * add test for instantiating RigidBody
    
    * add getters & setters to LinearMotionData & AngularMotionData
    
    * cleanup type annotations
    
    * add getters to MassData, InertiaData & MaterialData

--- src/rigid_body.jl
+++ src/rigid_body.jl
@@ -7,13 +7,16 @@ struct MassData{T}
     inv_mass::T
 end
 
-function MassData(density::T, shape::GB.GeometryPrimitive{2}) where {T<:Number}
+function MassData(density::T, shape::GB.GeometryPrimitive{2}) where {T<:AbstractFloat}
     mass = get_mass(density, shape)
     inv_mass = 1 / mass
-    MassData(mass, inv_mass)
+    MassData(convert(T, mass), convert(T, inv_mass))
 end
 
 MassData{T}() where {T} = MassData(one(T), one(T))
+
+get_mass(mass_data::MassData) = mass_data.mass
+get_inv_mass(mass_data::MassData) = mass_data.inv_mass
 
 #####
 # IntertiaData
@@ -24,35 +27,50 @@ struct InertiaData{T}
     inv_inertia::T
 end
 
-function InertiaData(density::T, shape::GB.GeometryPrimitive{2}) where {T<:Number}
+function InertiaData(density::T, shape::GB.GeometryPrimitive{2}) where {T<:AbstractFloat}
     inertia = get_inertia(density, shape)
     inv_inertia = 1 / inertia
-    InertiaData(inertia, inv_inertia)
+    InertiaData(convert(T, inertia), convert(T, inv_inertia))
 end
 
 InertiaData{T}() where {T} = InertiaData(one(T), one(T))
+
+get_inertia(inertia_data::InertiaData) = inertia_data.inertia
+get_inv_inertia(inertia_data::InertiaData) = inertia_data.inv_inertia
 
 #####
 # LinearMotionData
 #####
 
-struct LinearMotionData{T}
+mutable struct LinearMotionData{T}
     position::GB.Vec2{T}
     velocity::GB.Vec2{T}
 end
 
 LinearMotionData{T}() where {T} = LinearMotionData(zero(GB.Vec2{T}), zero(GB.Vec2{T}))
 
+get_velocity(linear_motion_data::LinearMotionData) = linear_motion_data.velocity
+set_velocity!(linear_motion_data::LinearMotionData, velocity) = linear_motion_data.velocity = velocity
+
+get_position(linear_motion_data::LinearMotionData) = linear_motion_data.position
+set_position!(linear_motion_data::LinearMotionData, position) = linear_motion_data.position = position
+
 #####
 # AngularMotionData
 #####
 
-struct AngularMotionData{T}
+mutable struct AngularMotionData{T}
     angle::T
     angular_velocity::T
 end
 
 AngularMotionData{T}() where {T} = AngularMotionData(zero(T), zero(T))
+
+get_angle(angular_motion_data::AngularMotionData) = angular_motion_data.angle
+set_angle!(angular_motion_data::AngularMotionData, angle) = angular_motion_data.angle = angle
+
+get_angular_velocity(angular_motion_data::AngularMotionData) = angular_motion_data.angular_velocity
+set_angular_velocity!(angular_motion_data::AngularMotionData, angular_velocity) = angular_motion_data.angular_velocity = angular_velocity
 
 #####
 # MaterialData
@@ -64,6 +82,9 @@ struct MaterialData{T}
 end
 
 MaterialData{T}() where {T} = MaterialData(one(T), one(T))
+
+get_density(material_data::MaterialData) = material_data.density
+get_restitution(material_data::MaterialData) = material_data.restitution
 
 #####
 # RigidBody
@@ -92,3 +113,9 @@ function RigidBody{T}() where {T<:AbstractFloat}
 
     return RigidBody(shape, material_data, mass_data, inertia_data, linear_motion_data, angular_motion_data, force, torque)
 end
+
+MacroTools.@forward RigidBody.linear_motion_data get_position, set_position!, get_velocity, set_velocity!
+MacroTools.@forward RigidBody.angular_motion_data get_angle, set_angle!, get_angular_velocity, set_angular_velocity!
+MacroTools.@forward RigidBody.mass_data get_mass, get_inv_mass
+MacroTools.@forward RigidBody.inertia_data get_inertia, get_inv_inertia
+MacroTools.@forward RigidBody.material_data get_density, get_restitution
