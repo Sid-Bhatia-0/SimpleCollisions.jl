b3cbfddad348a963f320f84c650a339e0b364e47
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-01-14 13:03:56 +0530

    Add basic run (#6)
    * make shape type a parameter
    
    * cleanup type annotations
    
    * remove parameter T from World struct
    
    * add test for World instantiation
    
    * add getters, setters, and update! for RigidBody
    
    * add update! for World
    
    * add basic run method for World

--- src/rigid_body.jl
+++ src/rigid_body.jl
@@ -7,7 +7,7 @@ struct MassData{T}
     inv_mass::T
 end
 
-function MassData(density::T, shape::GB.GeometryPrimitive{2}) where {T<:AbstractFloat}
+function MassData(density::T, shape::GB.AbstractGeometry) where {T<:AbstractFloat}
     mass = get_mass(density, shape)
     inv_mass = 1 / mass
     MassData(convert(T, mass), convert(T, inv_mass))
@@ -27,7 +27,7 @@ struct InertiaData{T}
     inv_inertia::T
 end
 
-function InertiaData(density::T, shape::GB.GeometryPrimitive{2}) where {T<:AbstractFloat}
+function InertiaData(density::T, shape::GB.AbstractGeometry) where {T<:AbstractFloat}
     inertia = get_inertia(density, shape)
     inv_inertia = 1 / inertia
     InertiaData(convert(T, inertia), convert(T, inv_inertia))
@@ -42,12 +42,12 @@ get_inv_inertia(inertia_data::InertiaData) = inertia_data.inv_inertia
 # LinearMotionData
 #####
 
-mutable struct LinearMotionData{T}
-    position::GB.Vec2{T}
-    velocity::GB.Vec2{T}
+mutable struct LinearMotionData{N, T}
+    position::GB.Vec{N, T}
+    velocity::GB.Vec{N, T}
 end
 
-LinearMotionData{T}() where {T} = LinearMotionData(zero(GB.Vec2{T}), zero(GB.Vec2{T}))
+LinearMotionData{N, T}() where {N, T} = LinearMotionData(zero(GB.Vec{N, T}), zero(GB.Vec{N, T}))
 
 get_velocity(linear_motion_data::LinearMotionData) = linear_motion_data.velocity
 set_velocity!(linear_motion_data::LinearMotionData, velocity) = linear_motion_data.velocity = velocity
@@ -55,22 +55,24 @@ set_velocity!(linear_motion_data::LinearMotionData, velocity) = linear_motion_da
 get_position(linear_motion_data::LinearMotionData) = linear_motion_data.position
 set_position!(linear_motion_data::LinearMotionData, position) = linear_motion_data.position = position
 
+const LinearMotionData2{T} = LinearMotionData{2, T}
+
 #####
-# AngularMotionData
+# AngularMotionData2
 #####
 
-mutable struct AngularMotionData{T}
+mutable struct AngularMotionData2{T}
     angle::T
     angular_velocity::T
 end
 
-AngularMotionData{T}() where {T} = AngularMotionData(zero(T), zero(T))
+AngularMotionData2{T}() where {T} = AngularMotionData2(zero(T), zero(T))
 
-get_angle(angular_motion_data::AngularMotionData) = angular_motion_data.angle
-set_angle!(angular_motion_data::AngularMotionData, angle) = angular_motion_data.angle = angle
+get_angle(angular_motion_data::AngularMotionData2) = angular_motion_data.angle
+set_angle!(angular_motion_data::AngularMotionData2, angle) = angular_motion_data.angle = angle
 
-get_angular_velocity(angular_motion_data::AngularMotionData) = angular_motion_data.angular_velocity
-set_angular_velocity!(angular_motion_data::AngularMotionData, angular_velocity) = angular_motion_data.angular_velocity = angular_velocity
+get_angular_velocity(angular_motion_data::AngularMotionData2) = angular_motion_data.angular_velocity
+set_angular_velocity!(angular_motion_data::AngularMotionData2, angular_velocity) = angular_motion_data.angular_velocity = angular_velocity
 
 #####
 # MaterialData
@@ -90,13 +92,13 @@ get_restitution(material_data::MaterialData) = material_data.restitution
 # RigidBody
 #####
 
-mutable struct RigidBody{T<:AbstractFloat}
-    shape::GB.GeometryPrimitive{2, T}
+mutable struct RigidBody{T<:AbstractFloat, S<:GB.AbstractGeometry{2, T}}
+    shape::S
     material_data::MaterialData{T}
     mass_data::MassData{T}
     inertia_data::InertiaData{T}
-    linear_motion_data::LinearMotionData{T}
-    angular_motion_data::AngularMotionData{T}
+    linear_motion_data::LinearMotionData2{T}
+    angular_motion_data::AngularMotionData2{T}
     force::GB.Vec2{T}
     torque::T
 end
@@ -106,12 +108,12 @@ function RigidBody{T}() where {T<:AbstractFloat}
     material_data = MaterialData{T}()
     mass_data = MassData(material_data.density, shape)
     inertia_data = InertiaData(material_data.density, shape)
-    linear_motion_data = LinearMotionData{T}()
-    angular_motion_data = AngularMotionData{T}()
+    linear_motion_data = LinearMotionData2{T}()
+    angular_motion_data = AngularMotionData2{T}()
     force = zero(GB.Vec2{T})
     torque = zero(T)
 
-    return RigidBody(shape, material_data, mass_data, inertia_data, linear_motion_data, angular_motion_data, force, torque)
+    return RigidBody{T, typeof(shape)}(shape, material_data, mass_data, inertia_data, linear_motion_data, angular_motion_data, force, torque)
 end
 
 MacroTools.@forward RigidBody.linear_motion_data get_position, set_position!, get_velocity, set_velocity!
@@ -119,3 +121,31 @@ MacroTools.@forward RigidBody.angular_motion_data get_angle, set_angle!, get_ang
 MacroTools.@forward RigidBody.mass_data get_mass, get_inv_mass
 MacroTools.@forward RigidBody.inertia_data get_inertia, get_inv_inertia
 MacroTools.@forward RigidBody.material_data get_density, get_restitution
+
+get_shape(body::RigidBody) = body.shape
+set_shape!(body::RigidBody, shape) = body.shape = shape
+get_shape(shape::GB.HyperSphere, position) = typeof(shape)(GB.Point(position), shape.r)
+get_shape(shape::GB.HyperRectangle, position) = typeof(shape)(position..., shape.widths)
+
+get_force(body::RigidBody) = body.force
+set_force!(body::RigidBody, force) = body.force = force
+get_torque(body::RigidBody) = body.torque
+set_torque!(body::RigidBody, torque) = body.torque = torque
+
+function update!(body::RigidBody, dt)
+    inv_mass = get_inv_mass(body)
+    force = get_force(body)
+    velocity = get_velocity(body)
+    new_velocity = velocity + inv_mass * force * dt
+    set_velocity!(body, new_velocity)
+
+    position = get_position(body)
+    new_position = position + new_velocity * dt
+    set_position!(body, new_position)
+
+    shape = get_shape(body)
+    new_shape = get_shape(shape, new_position)
+    set_shape!(body, new_shape)
+
+    return body
+end
--- src/world.jl
+++ src/world.jl
@@ -1,3 +1,18 @@
-mutable struct World{T}
-    bodies::Vector{RigidBody{T}}
+mutable struct World
+    bodies::Vector{RigidBody}
+end
+
+function update!(world::World, dt)
+    for body in world.bodies
+        update!(body, dt)
+    end
+end
+
+function Base.run(world::World, num_iter, dt)
+
+    for i in 1:num_iter
+        update!(world, dt)
+    end
+
+    return world
 end
