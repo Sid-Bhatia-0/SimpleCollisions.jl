9fe30d00be15fc1a659cf9cb2be6d5c316637a56
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-01-15 23:49:58 +0530

    render simulation with motion

--- src/rendering.jl
+++ src/rendering.jl
@@ -2,12 +2,20 @@ import .Makie
 
 function init_screen(world_node::Makie.Observable{<:World}; resolution = (720, 720))
     scene = Makie.Scene(resolution = resolution)
-    Makie.poly!(scene, [body.shape for body in world_node[].bodies])
+    shapes_node = Makie.lift(world_node) do world
+        shapes = get_shape.(get_bodies(world))
+    end
+    Makie.poly!(shapes_node)
     return scene
 end
 
-function render(world::World)
+function render!(world::World; num_iter = 100, dt = 0.01, frame_rate = 24)
     world_node = Makie.Node(world)
     scene = init_screen(world_node)
     Makie.display(scene)
+    for i in 1:num_iter
+        world_node[] = update!(world_node[], dt)
+        sleep(1 / frame_rate)
+    end
+    return scene
 end
