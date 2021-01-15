101dbe20af4ba8289fb292ff5e1d19f8432a6abd
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-01-15 23:46:55 +0530

    add get_bodies method, & return world in update!

--- src/world.jl
+++ src/world.jl
@@ -2,17 +2,18 @@ mutable struct World
     bodies::Vector{RigidBody}
 end
 
+get_bodies(world::World) = world.bodies
+
 function update!(world::World, dt)
     for body in world.bodies
         update!(body, dt)
     end
+    return world
 end
 
 function Base.run(world::World, num_iter, dt)
-
     for i in 1:num_iter
         update!(world, dt)
     end
-
     return world
 end
