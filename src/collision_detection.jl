788456fa11fb6a95c337821d80c8667152fba61f
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-01-16 13:54:03 +0530

    cleanup the order of is_colliding methods

--- src/collision_detection.jl
+++ src/collision_detection.jl
@@ -1,11 +1,12 @@
-function is_colliding(a::GB.HyperRectangle{N}, b::GB.HyperRectangle{N}) where {N}
-    for i in 1:N
-        if (a.origin[i] + a.widths[i] < b.origin[i]) || (b.origin[i] + b.widths[i] < a.origin[i])
-            return false
-        end
-    end
-    return true
-end
+#####
+# Point vs. Point
+#####
+
+is_colliding(a::GB.Point{N}, b::GB.Point{N}) where {N} = a == b
+
+#####
+# HyperSphere vs. Point
+#####
 
 function is_colliding(a::GB.HyperSphere{N}, b::GB.Point{N}) where {N}
     ba = b .- a.center
@@ -14,10 +15,18 @@ end
 
 is_colliding(a::GB.Point{N}, b::GB.HyperSphere{N}) where {N} = is_colliding(b, a)
 
+#####
+# HyperSphere vs. HyperSphere
+#####
+
 function is_colliding(a::GB.HyperSphere{N}, b::GB.HyperSphere{N}) where {N}
     ba = b.center .- a.center
     return LA.dot(ba, ba) <= (a.r + b.r) ^ 2
 end
+
+#####
+# HyperRectangle vs. HyperSphere
+#####
 
 function is_colliding(a::GB.HyperRectangle{N}, b::GB.HyperSphere{N}) where {N}
     a_center = get_center(a)
@@ -32,3 +41,16 @@ function is_colliding(a::GB.HyperRectangle{N}, b::GB.HyperSphere{N}) where {N}
 end
 
 is_colliding(a::GB.HyperSphere{N}, b::GB.HyperRectangle{N}) where {N} = is_colliding(b, a)
+
+#####
+# HyperRectangle vs. HyperRectangle
+#####
+
+function is_colliding(a::GB.HyperRectangle{N}, b::GB.HyperRectangle{N}) where {N}
+    for i in 1:N
+        if (a.origin[i] + a.widths[i] < b.origin[i]) || (b.origin[i] + b.widths[i] < a.origin[i])
+            return false
+        end
+    end
+    return true
+end
