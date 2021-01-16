45bc85a4e4277cf8ac025b755059633029f1bd59
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-01-16 13:59:10 +0530

    cleanup runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,86 +3,90 @@ using GeometryBasics
 using Test
 
 @testset "PhysicsEngine2D.jl" begin
-    @testset "Rect2D area" begin
-        a = Rect2D(1, 2, 3, 4)
-        @test area(a) == 12
+    @testset "area computation" begin
+        @testset "Rect2D" begin
+            a = Rect2D(1, 2, 3, 4)
+            @test area(a) == 12
+        end
+
+        @testset "Circle" begin
+            a = Circle(Point2(0, 0), 1)
+            @test area(a) ≈ π
+        end
     end
 
-    @testset "Circle area" begin
-        a = Circle(Point2(0, 0), 1)
-        @test area(a) ≈ π
-    end
+    @testset "area computation" begin
+        @testset "Circle vs. Point" begin
+            a = Circle(Point2(0, 0), 1)
+            b = Point(0, 0)
+            @test PE2D.is_colliding(a, b) == true
 
-    @testset "Rect2D vs. Rect2D collision" begin
-        a = Rect2D(1, 2, 3, 4)
-        b = Rect2D(3, 4, 5, 6)
-        @test PE2D.is_colliding(a, b) == true
+            a = Circle(Point2(0, 0), 1)
+            b = Point(1, 0)
+            @test PE2D.is_colliding(a, b) == true
 
-        a = Rect2D(1, 2, 3, 4)
-        b = Rect2D(4, 6, 1, 2)
-        @test PE2D.is_colliding(a, b) == true
+            a = Circle(Point2(0, 0), 1)
+            b = Point(2, 0)
+            @test PE2D.is_colliding(a, b) == false
+        end
 
-        a = Rect2D(1, 2, 3, 4)
-        b = Rect2D(0, 0, 6, 6)
-        @test PE2D.is_colliding(a, b) == true
+        @testset "Circle vs. Circle" begin
+            a = Circle(Point2(0, 0), 1)
+            b = Circle(Point2(0, 0), 1)
+            @test PE2D.is_colliding(a, b) == true
 
-        a = Rect2D(1, 2, 3, 4)
-        b = Rect2D(4, 2, 1, 2)
-        @test PE2D.is_colliding(a, b) == true
+            a = Circle(Point2(0, 0), 1)
+            b = Circle(Point2(2, 0), 1)
+            @test PE2D.is_colliding(a, b) == true
 
-        a = Rect2D(1, 2, 3, 4)
-        b = Rect2D(5, 6, 7, 8)
-        @test PE2D.is_colliding(a, b) == false
-    end
+            a = Circle(Point2(0, 0), 1)
+            b = Circle(Point2(3, 0), 1)
+            @test PE2D.is_colliding(a, b) == false
+        end
 
-    @testset "Circle vs. Circle collision" begin
-        a = Circle(Point2(0, 0), 1)
-        b = Circle(Point2(0, 0), 1)
-        @test PE2D.is_colliding(a, b) == true
+        @testset "Rect2D vs. Circle" begin
+            a = Rect2D(1, 2, 5, 6)
+            b = Circle(Point2(3, 3), 1)
+            @test PE2D.is_colliding(a, b) == true
 
-        a = Circle(Point2(0, 0), 1)
-        b = Circle(Point2(2, 0), 1)
-        @test PE2D.is_colliding(a, b) == true
+            a = Rect2D(1, 2, 5, 6)
+            b = Circle(Point2(4, 4), 1)
+            @test PE2D.is_colliding(a, b) == true
 
-        a = Circle(Point2(0, 0), 1)
-        b = Circle(Point2(3, 0), 1)
-        @test PE2D.is_colliding(a, b) == false
-    end
+            a = Rect2D(1, 2, 5, 6)
+            b = Circle(Point2(4, 4), 10)
+            @test PE2D.is_colliding(a, b) == true
 
-    @testset "Circle vs. Point collision" begin
-        a = Circle(Point2(0, 0), 1)
-        b = Point(0, 0)
-        @test PE2D.is_colliding(a, b) == true
+            a = Rect2D(1, 2, 5, 6)
+            b = Circle(Point2(0, 0), 3)
+            @test PE2D.is_colliding(a, b) == true
 
-        a = Circle(Point2(0, 0), 1)
-        b = Point(1, 0)
-        @test PE2D.is_colliding(a, b) == true
+            a = Rect2D(1, 2, 5, 6)
+            b = Circle(Point2(0, 0), 1)
+            @test PE2D.is_colliding(a, b) == false
+        end
 
-        a = Circle(Point2(0, 0), 1)
-        b = Point(2, 0)
-        @test PE2D.is_colliding(a, b) == false
-    end
+        @testset "Rect2D vs. Rect2D" begin
+            a = Rect2D(1, 2, 3, 4)
+            b = Rect2D(3, 4, 5, 6)
+            @test PE2D.is_colliding(a, b) == true
 
-    @testset "Rect2D vs. Circle collision" begin
-        a = Rect2D(1, 2, 5, 6)
-        b = Circle(Point2(3, 3), 1)
-        @test PE2D.is_colliding(a, b) == true
+            a = Rect2D(1, 2, 3, 4)
+            b = Rect2D(4, 6, 1, 2)
+            @test PE2D.is_colliding(a, b) == true
 
-        a = Rect2D(1, 2, 5, 6)
-        b = Circle(Point2(4, 4), 1)
-        @test PE2D.is_colliding(a, b) == true
+            a = Rect2D(1, 2, 3, 4)
+            b = Rect2D(0, 0, 6, 6)
+            @test PE2D.is_colliding(a, b) == true
 
-        a = Rect2D(1, 2, 5, 6)
-        b = Circle(Point2(4, 4), 10)
-        @test PE2D.is_colliding(a, b) == true
+            a = Rect2D(1, 2, 3, 4)
+            b = Rect2D(4, 2, 1, 2)
+            @test PE2D.is_colliding(a, b) == true
 
-        a = Rect2D(1, 2, 5, 6)
-        b = Circle(Point2(0, 0), 3)
-        @test PE2D.is_colliding(a, b) == true
-
-        a = Rect2D(1, 2, 5, 6)
-        b = Circle(Point2(0, 0), 1)
-        @test PE2D.is_colliding(a, b) == false
+            a = Rect2D(1, 2, 3, 4)
+            b = Rect2D(5, 6, 7, 8)
+            @test PE2D.is_colliding(a, b) == false
+        end
     end
 
     @testset "RigidBody instantiation" begin
