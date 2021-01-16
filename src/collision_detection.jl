940b3935de19bb38696b010988615e47529b30bb
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-01-16 13:29:26 +0530

    generalize is_colliding to HyperSphere, HyperRectangle

--- src/collision_detection.jl
+++ src/collision_detection.jl
@@ -1,14 +1,13 @@
-function is_colliding(a::GB.Rect2D, b::GB.Rect2D)
-    if (a.origin[1] + a.widths[1] < b.origin[1]) || (b.origin[1] + b.widths[1] < a.origin[1])
-        return false
-    end
-    if (a.origin[2] + a.widths[2] < b.origin[2]) || (b.origin[2] + b.widths[2] < a.origin[2])
-        return false
+function is_colliding(a::GB.HyperRectangle{N}, b::GB.HyperRectangle{N}) where {N}
+    for i in 1:N
+        if (a.origin[i] + a.widths[i] < b.origin[i]) || (b.origin[i] + b.widths[i] < a.origin[i])
+            return false
+        end
     end
     return true
 end
 
-function is_colliding(a::GB.Circle, b::GB.Circle)
+function is_colliding(a::GB.HyperSphere{N}, b::GB.HyperSphere{N}) where {N}
     ba = b.center .- a.center
     return LA.dot(ba, ba) <= (a.r + b.r) ^ 2
 end
