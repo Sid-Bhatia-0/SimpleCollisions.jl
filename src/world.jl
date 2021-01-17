cf50d3a5499b0b4b6008d4eaca7c72a8875a7d10
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-01-17 17:59:44 +0530

    complete update!

--- src/world.jl
+++ src/world.jl
@@ -1,4 +1,4 @@
-mutable struct World
+struct World
     bodies::Vector{RigidBody}
 end
 
@@ -7,9 +7,47 @@ get_bodies(world::World) = world.bodies
 @pretty_print World
 
 function update!(world::World, dt)
-    for body in world.bodies
-        update!(body, dt)
+    bodies = get_bodies(world)
+    num_bodies = length(bodies)
+
+    # updates caused by pair-wise interaction of rigid bodies
+    for i in 1:num_bodies-1
+        for j in i+1:num_bodies
+            a = bodies[i]
+            shape_a = get_shape(a)
+            b = bodies[j]
+            shape_b = get_shape(b)
+            if is_colliding(shape_a, shape_b)
+                display(world)
+                manifold_ba = Manifold(shape_a, shape_b)
+                velocity_change_a, velocity_change_b = resolve_collision!(a, b, manifold_ba)
+                add_velocity_change!(a, velocity_change_a)
+                apply_velocity_change!(a)
+                add_velocity_change!(b, velocity_change_b)
+                apply_velocity_change!(b)
+            end
+        end
     end
+
+    # updates caused by self (individual body)
+    for body in bodies
+        # accumulate velocity change caused by net force
+        inv_mass = get_inv_mass(body)
+        force = get_force(body)
+        add_velocity_change!(body, inv_mass * force * dt)
+        apply_velocity_change!(body)
+
+        # accumulate position change caused by net velocity
+        velocity = get_velocity(body)
+        add_position_change!(body, velocity * dt)
+        apply_position_change!(body)
+
+        # update shape caused by change in position
+        shape = get_shape(body)
+        position = get_position(body)
+        set_shape!(body, translated(shape, position))
+    end
+
     return world
 end
 
@@ -17,5 +55,6 @@ function Base.run(world::World, num_iter, dt)
     for i in 1:num_iter
         update!(world, dt)
     end
+
     return world
 end
