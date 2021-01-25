3a6443fce859fb15c05798e18d00e5f4b3c16684
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-01-26 03:14:55 +0530

    add get_area for triangle formed by 3 points

--- src/shapes.jl
+++ src/shapes.jl
@@ -4,6 +4,15 @@ get_half_widths(a::GB.HyperRectangle) = a.widths ./ 2
 
 GB.area(a::GB.Rect2D) = prod(a.widths)
 GB.area(a::GB.Circle) = π * a.r * a.r
+function get_area(p1::GB.Point2, p2::GB.Point2, p3::GB.Point2)
+    x1 = p1[1]
+    y1 = p1[2]
+    x2 = p2[1]
+    y2 = p2[2]
+    x3 = p3[1]
+    y3 = p3[2]
+    return abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
+end
 
 get_mass(density::Number, shape::GB.GeometryPrimitive{2}) = density * GB.area(shape)
 
