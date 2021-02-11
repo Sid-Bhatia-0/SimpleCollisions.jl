6c40e3b8de8fd28d70983a3f78bf3eac1bb03b53
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-02-12 02:01:15 +0530

    use Std shapes instead of GB shapes

--- src/manifold.jl
+++ src/manifold.jl
@@ -1,7 +1,7 @@
 struct Manifold{T}
     penetration::T
     axes::Axes{T}
-    contact::GB.Vec2{T}
+    contact::GB.Vec{2, T}
 end
 
 get_penetration(manifold::Manifold) = manifold.penetration
@@ -10,95 +10,82 @@ get_normal(manifold::Manifold) = manifold |> get_axes |> get_y_cap
 get_tangent(manifold::Manifold) = manifold |> get_axes |> get_x_cap
 get_contact(manifold::Manifold) = manifold.contact
 
-function Manifold(a::RigidBody, b::RigidBody)
-    shape_a = get_shape(a)
-    shape_b = get_shape(b)
-
-    pos_a = get_position(a)
-    pos_b = get_position(b)
-    pos_ba = position_b .- position_a
-
-    axes_a = get_axes(a)
-    axes_b = get_axes(b)
-    axes_ba = get_relative_axes(axes_a, axes_b)
-
-    return Manifold(shape_a, shape_b, pos_ba, axes_ba)
-end
-
 #####
-# HyperSphere vs. HyperSphere
+# StdCircle vs. StdCircle
 #####
 
-function Manifold(a::GB.HyperSphere, b::GB.HyperSphere, pos_ba)
+function Manifold(a::StdCircle{T}, b::StdCircle{T}, pos_ba::GB.Vec{2, T}) where {T}
+    r_a = get_radius(a)
+    r_b = get_radius(b)
     normal = LA.normalize(pos_ba)
     if all(isfinite.(normal))
-        d = LA.norm(pos_ba)
-        penetration = a.r + b.r - d
+        penetration = r_a + r_b - LA.norm(pos_ba)
         tangent = rotate_minus_90(normal)
         axes = Axes(tangent, normal)
-        contact = (a.r - penetration / 2) .* normal
+        contact = (r_a - penetration / 2) .* normal
         return Manifold(penetration, axes, contact)
     else
-        penetration = a.r + b.r
-        T = eltype(pos_ba)
+        penetration = r_a + r_b
         axes = rotate_minus_90(Axes{T}())
-        tangent = get_x_cap(axes)
-        normal = get_y_cap(axes)
-        contact = (a.r - penetration / 2) .* normal
+        normal = get_x_cap(axes)
+        contact = (r_a - penetration / 2) .* normal
         return Manifold(penetration, axes, contact)
     end
 end
 
-Manifold(a::GB.HyperSphere, b::GB.HyperSphere, pos_ba, axes_ba) = Manifold(a, b, pos_ba)
+Manifold(a::StdCircle{T}, b::StdCircle{T}, pos_ba::GB.Vec{2, T}, axes_ba::Axes{T}) where {T} = Manifold(a, b, pos_ba)
 
 #####
 # HyperRectangle vs. HyperSphere
 #####
 
-function project_from_inside(a::GB.HyperRectangle{N}, b::GB.Vec{N}) where {N}
-    bottom_left = get_bottom_left(a)
-    top_right = get_top_right(a)
-    min_dist = Inf
-    min_dist_dim = 0
-    min_dist_dir = 0
+function get_closest_edge_from_inside(rect::StdRect{T}, pos::GB.Vec{2, T}) where {T}
+    half_width = get_half_width(rect)
+    half_height = get_half_height(rect)
 
-    for i in 1:N
-        dist, dir = findmin((b[i] - bottom_left[i], top_right[i] - b[i]))
-        if dist < min_dist
-            min_dist = dist
-            min_dist_dim = i
-            min_dist_dir = dir == 1 ? -1 : 1
-        end
-    end
+    x = get_x(pos)
+    y = get_y(pos)
 
-    return (min_dist, min_dist_dim, min_dist_dir)
+    penetration, edge_id = findmin((y + half_height, half_width - x, half_height - y, x + half_width))
+
+    return (penetration, edge_id)
 end
 
-function Manifold(a::GB.Rect, b::GB.HyperSphere, pos_ba::GB.Vec)
-    closest_point_ba = project(a, b, pos_ba)
-    vec = pos_ba .- closest_point_ba
+function Manifold(a::StdRect{T}, b::StdCircle{T}, pos_ba::GB.Vec{2, T}) where {T}
+    r_b = get_radius(b)
+    projection = get_projection(a, pos_ba)
+    vec = pos_ba .- projection
     normal = LA.normalize(vec)
     if all(isfinite.(normal))
-        d = LA.norm(vec)
-        penetration = b.r - d
+        penetration = r_b - LA.norm(vec)
         tangent = rotate_minus_90(normal)
         axes = Axes(tangent, normal)
-        contact = pos_ba .+ (b.r - penetration / 2) .* -normal
+        contact = pos_ba .+ (r_b - penetration / 2) .* -normal
         return Manifold(penetration, axes, contact)
     else
-        dist, dim, dir = project_from_inside(a, pos_ba)
-        penetration = b.r + dist
-        normal = dir * GB.unit(typeof(pos_ba), dim)
+        d, edge_id = get_closest_edge_from_inside(a, pos_ba)
+        penetration = r_b + d
+        normal = get_normals(a)[edge_id]
         tangent = rotate_minus_90(normal)
         axes = Axes(tangent, normal)
-        contact = pos_ba .+ (b.r - penetration / 2) .* -normal
+        contact = pos_ba .+ (r_b - penetration / 2) .* -normal
         return Manifold(penetration, axes, contact)
     end
 end
 
-Manifold(a::GB.Rect, b::GB.HyperSphere, pos_ba, axes_ba) = Manifold(a, b, pos_ba)
+function Manifold(a::StdCircle{T}, b::StdRect{T}, pos_ba::GB.Vec{2, T}) where {T}
+    pos_ab = -pos_ba
+    manifold_ab = Manifold(b, a, pos_ab)
 
-function Manifold(a::GB.HyperSphere, b::GB.Rect, pos_ba, axes_ba)
+    penetration = get_penetration(manifold_ab)
+    axes = manifold_ab |> get_axes |> rotate_180
+    contact = pos .+ contact
+    return Manifold(penetration, axes, contact)
+end
+
+Manifold(a::StdRect{T}, b::StdCircle{T}, pos_ba::GB.Vec{2, T}, axes_ba::Axes{T}) where {T} = Manifold(a, b, pos_ba)
+
+function Manifold(a::StdCircle{T}, b::StdRect{T}, pos_ba::GB.Vec{2, T}, axes_ba::Axes{T}) where {T}
     axes_ab = invert_relative_axes(axes_ba)
     pos_ab = -rotate(pos_ba, axes_ab)
     manifold_ab = Manifold(b, a, pos_ab, axes_ab)
@@ -110,16 +97,15 @@ function Manifold(a::GB.HyperSphere, b::GB.Rect, pos_ba, axes_ba)
 end
 
 #####
-# HyperRectangle vs. HyperRectangle
+# StdRect vs. StdRect
 #####
 
-function get_clipped_vertices(a::GB.Rect, b::GB.Rect, pos_ba, axes_ba)
-    half_widths_a = get_top_right(a)
-    half_width_a = half_widths_a[1]
-    half_height_a = half_widths_a[2]
+function get_clipped_vertices(a::StdRect{T}, b::StdRect{T}, pos_ba::GB.Vec{2, T}, axes_ba::Axes{T}) where {T}
+    half_width_a = get_half_width(a)
+    half_height_a = get_half_height(a)
 
     vertices_ba = get_vertices(b, pos_ba, axes_ba)
-    VecType = typeof(vertices_ba[1])
+    VecType = typeof(pos_ba)
 
     initial_vertices = (vertices_ba..., vertices_ba[1])
     final_vertices = VecType[]
@@ -276,13 +262,14 @@ function get_clipped_vertices(a::GB.Rect, b::GB.Rect, pos_ba, axes_ba)
     return final_vertices
 end
 
-function get_centroid(augmented_vertices...)
-    VecType = typeof(augmented_vertices[1])
-    T = eltype(augmented_vertices[1])
+function get_centroid(vertices::Vararg{GB.Vec{2, T}}) where {T}
+    VecType = typeof(vertices[1])
     area = zero(T)
     c_x_num = zero(T)
     c_y_num = zero(T)
     den = zero(T)
+
+    augmented_vertices = (vertices..., vertices[1])
 
     for i in 1:length(augmented_vertices) - 1
         v_i = augmented_vertices[i]
@@ -305,35 +292,38 @@ function get_centroid(augmented_vertices...)
     if isfinite(c_x) && isfinite(c_y)
         return VecType(c_x, c_y)
     else
-        return reduce(+, augmented_vertices[1:end-1]) ./ (length(augmented_vertices) - 1)
+        centroid = zero(VecType)
+        for vertex in vertices
+            centroid = centroid .+ vertex
+        end
+        return centroid / length(vertices)
     end
 end
 
-function get_contact(a::GB.Rect, b::GB.Rect, pos_ba, axes_ba)
+function get_contact(a::StdRect{T}, b::StdRect{T}, pos_ba::GB.Vec{2, T}, axes_ba::Axes{T}) where {T}
     clipped_vertices = get_clipped_vertices(a, b, pos_ba, axes_ba)
-    return get_centroid(clipped_vertices..., clipped_vertices[1])
+    return get_centroid(clipped_vertices...)
 end
 
-function get_candidate_support(a::GB.Rect, b::GB.Rect, pos_ba, axes_ba)
-    half_widths_a = get_top_right(a)
-    half_width_a = half_widths_a[1]
-    half_height_a = half_widths_a[2]
+function get_candidate_support(a::StdRect{T}, b::StdRect{T}, pos_ba::GB.Vec{2, T}, axes_ba::Axes{T}) where {T}
+    half_width_a = get_half_width(a)
+    half_height_a = get_half_height(a)
 
     vertices_ba = get_vertices(b, pos_ba, axes_ba)
 
-    value_1, vertex_1, vertex_id_1 = findmax(vertex -> vertex[2], vertices_ba)
+    value_1, _, vertex_id_1 = findmax(vertex -> vertex[2], vertices_ba)
     max_penetration_1 = half_height_a + value_1
 
-    value_2, vertex_2, vertex_id_2 = findmin(vertex -> vertex[1], vertices_ba)
+    value_2, _, vertex_id_2 = findmin(vertex -> vertex[1], vertices_ba)
     max_penetration_2 = half_width_a - value_2
 
-    value_3, vertex_3, vertex_id_3 = findmin(vertex -> vertex[2], vertices_ba)
+    value_3, _, vertex_id_3 = findmin(vertex -> vertex[2], vertices_ba)
     max_penetration_3 = half_height_a - value_3
 
-    value_4, vertex_4, vertex_id_4 = findmax(vertex -> vertex[1], vertices_ba)
+    value_4, _, vertex_id_4 = findmax(vertex -> vertex[1], vertices_ba)
     max_penetration_4 = half_width_a + value_4
 
-    penetration, (max_penetration, vertex_id), edge_id = findmin(x -> x[1], ((max_penetration_1, vertex_id_1),
+    penetration, (_, vertex_id), edge_id = findmin(x -> x[1], ((max_penetration_1, vertex_id_1),
                                           (max_penetration_2, vertex_id_2),
                                           (max_penetration_3, vertex_id_3),
                                           (max_penetration_4, vertex_id_4)))
@@ -341,11 +331,10 @@ function get_candidate_support(a::GB.Rect, b::GB.Rect, pos_ba, axes_ba)
     return penetration, vertex_id, edge_id
 end
 
-function Manifold(a::GB.Rect, b::GB.Rect, pos_ba, axes_ba)
+function Manifold(a::StdRect{T}, b::StdRect{T}, pos_ba::GB.Vec{2, T}, axes_ba::Axes{T}) where {T}
     penetration_ba, vertex_id_b, edge_id_a = get_candidate_support(a, b, pos_ba, axes_ba)
 
-    axes_ab = invert_relative_axes(axes_ba)
-    pos_ab = -rotate(pos_ba, axes_ab)
+    pos_ab, axes_ab = invert(pos_ba, axes_ba)
     penetration_ab, vertex_id_a, edge_id_b = get_candidate_support(b, a, pos_ab, axes_ab)
 
     contact = get_contact(a, b, pos_ba, axes_ba)
