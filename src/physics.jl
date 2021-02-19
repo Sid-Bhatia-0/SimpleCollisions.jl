0a1d88a9c5e0f268e4f0a4d1e2a97f5c64e8499e
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-02-19 22:07:07 +0530

    add physics primitives (#21)
    * add get_plotting_shape method for rendering
    
    * add basic physics getters & setters <:AbstractBody
    
    * add impulse calculation methods for physics
    
    * load physics and rendering related files

---
+++ src/physics.jl
@@ -0,0 +1,32 @@
+# dynamic body vs. dynamic body
+function get_normal_impulse(inv_mass_a::T, inv_mass_b::T, initial_velocity_ao::GB.Vec{2, T}, initial_velocity_bo::GB.Vec{2, T}, e::T, normal_o::GB.Vec{2, T}) where {T}
+    initial_velocity_ba = initial_velocity_bo .- initial_velocity_ao
+    initial_velocity_ba_normal_o = LA.dot(initial_velocity_ba, normal_o)
+    final_velocity_ba_normal_o = max(zero(T), -e * initial_velocity_ba_normal_o)
+    j_ao_normal_o = (initial_velocity_ba_normal_o - final_velocity_ba_normal_o) / (inv_mass_a + inv_mass_b)
+    j_bo_normal_o = -j_ao_normal_o
+    return j_ao_normal_o, j_bo_normal_o
+end
+
+# dynamic body vs. kinematic body
+get_normal_impulse(inv_mass_b::T, initial_velocity_ao::GB.Vec{2, T}, initial_velocity_bo::GB.Vec{2, T}, e::T, normal_o::GB.Vec{2, T}) where {T} = get_normal_impulse(zero(T), inv_mass_b, initial_velocity_ao, initial_velocity_bo, e, normal_o)
+
+# dynamic body vs. static body
+get_normal_impulse(inv_mass_b::T, initial_velocity_bo::GB.Vec{2, T}, e::T, normal_o::GB.Vec{2, T}) where {T} = get_normal_impulse(zero(T), inv_mass_b, zero(GB.Vec{2, T}), initial_velocity_bo, e, normal_o)
+
+function get_tangential_impulse(inv_mass_a::T, inv_mass_b::T, initial_velocity_ao::GB.Vec{2, T}, initial_velocity_ba::GB.Vec{2, T}, tangent_o::GB.Vec{2, T}, mu_s::T, mu_k::T, j_ao_normal_o::T) where {T}
+    initial_velocity_ba = initial_velocity_bo .- initial_velocity_ao
+    initial_velocity_ba_tangent_o = LA.dot(initial_velocity_ba, tangent_o)
+
+    max_j_ao_tangent_o = initial_velocity_ba_tangent_o / (inv_mass_a + inv_mass_b)
+
+    if abs(max_j_ao_tangent_o) <= abs(mu_s * j_ao_normal_o)
+        j_ao_tangent_o = max_j_ao_tangent_o
+        j_bo_tangent_o = -j_ao_tangent_o
+        return j_ao_tangent_o, j_bo_tangent_o
+    else
+        j_ao_tangent_o = sign(max_j_ao_tangent_o) * abs(mu_k * j_ao_normal_o)
+        j_bo_tangent_o = -j_ao_tangent_o
+        return j_ao_tangent_o, j_bo_tangent_o
+    end
+end
