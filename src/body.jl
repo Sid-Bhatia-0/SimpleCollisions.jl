aa4e89af13bee9cf4092a08c6a2409e0e037fb2e
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-02-19 21:59:32 +0530

    add basic physics getters & setters <:AbstractBody

---
+++ src/body.jl
@@ -0,0 +1,35 @@
+#####
+# Body
+#####
+
+abstract type AbstractBody{T} end
+
+get_shape(body::AbstractBody) = body.shape
+
+get_position(body::AbstractBody) = body.position
+set_position!(body::AbstractBody{T}, position::GB.Vec{2, T}) where {T} = body.position = position
+get_position_change(body::AbstractBody) = body.position_change
+set_position_change!(body::AbstractBody{T}, position_change::GB.Vec{2, T}) where {T} = body.position_change = position_change
+add_position_change!(body::AbstractBody{T}, position_change::GB.Vec{2, T}) where {T} = set_position_change!(body, get_position_change(body) .+ position_change)
+function apply_position_change!(body)
+    position = get_position(body)
+    position_change = get_position_change(body)
+    set_position!(body, position .+ position_change)
+    set_position_change!(body, zero(position_change))
+    return body
+end
+
+get_velocity(body::AbstractBody) = body.velocity
+set_velocity!(body::AbstractBody{T}, velocity::GB.Vec{2, T}) where {T} = body.velocity = velocity
+get_velocity_change(body::AbstractBody) = body.velocity_change
+set_velocity_change!(body::AbstractBody{T}, velocity_change::GB.Vec{2, T}) where {T} = body.velocity_change = velocity_change
+add_velocity_change!(body::AbstractBody{T}, velocity_change::GB.Vec{2, T}) where {T} = set_velocity_change!(body, get_velocity_change(body) .+ velocity_change)
+function apply_velocity_change!(body)
+    velocity = get_velocity(body)
+    velocity_change = get_velocity_change(body)
+    set_velocity!(body, velocity .+ velocity_change)
+    set_velocity_change!(body, zero(velocity_change))
+    return body
+end
+
+get_inv_mass(body::AbstractBody) = body.inv_mass
