1bd994f82c1909127d1d19c213ad80603f316450
Sid-Bhatia-0 <sidb60200@gmail.com>
2021-03-06 17:00:58 +0530

    remove Axes from position_and_orientation.jl & shapes.jl

--- src/position_and_orientation.jl
+++ src/position_and_orientation.jl
@@ -1,7 +1,3 @@
-#####
-# Vec
-#####
-
 get_x(vec::SA.SVector) = vec[1]
 get_y(vec::SA.SVector) = vec[2]
 
@@ -11,63 +7,13 @@ rotate_180(vec::SA.SVector{2}) = -vec
 
 rotate(x::T, y::T, c::T, s::T) where {T} = SA.SVector(c * x - s * y, s * x + c * y)
 rotate(vec::SA.SVector{2, T}, theta::T) where {T} = rotate(vec[1], vec[2], cos(theta), sin(theta))
-
-#####
-# Axes
-#####
-
-struct Axes{T}
-    x_cap::SA.SVector{2, T}
-    y_cap::SA.SVector{2, T}
-end
-
-get_x_cap(axes::Axes) = axes.x_cap
-get_y_cap(axes::Axes) = axes.y_cap
-
-function Axes{T}() where {T}
-    x_cap = SA.SVector(one(T), zero(T))
-    y_cap = SA.SVector(zero(T), one(T))
-    return Axes(x_cap, y_cap)
-end
-
-function Axes(angle::T) where {T}
-    x_cap = SA.SVector(cos(angle), sin(angle))
-    y_cap = rotate_plus_90(x_cap)
-    return Axes(x_cap, y_cap)
-end
-
-rotate_plus_90(axes::Axes) = Axes(get_y_cap(axes), -get_x_cap(axes))
-rotate_180(axes::Axes) = Axes(-get_x_cap(axes), -get_y_cap(axes))
-rotate_minus_90(axes::Axes) = Axes(-get_y_cap(axes), get_x_cap(axes))
-
-function rotate(vec::SA.SVector{2, T}, axes::Axes{T}) where {T}
-    x_cap = get_x_cap(axes)
-    c = x_cap[1]
-    s = x_cap[2]
-    return rotate(vec[1], vec[2], c, s)
-end
-
-#####
-# relative orientation
-#####
+rotate(vec::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T} = rotate(vec[1], vec[2], dir[1], dir[2])
 
 get_relative_direction(dir1::SA.SVector{2, T}, dir2::SA.SVector{2, T}) where {T} = typeof(dir1)(dir2[1] * dir1[1] + dir2[2] * dir1[2], dir2[2] * dir1[1] - dir2[1] * dir1[2])
 invert_relative_direction(dir::SA.SVector{2}) = typeof(dir)(dir[1], -dir[2])
 
-function get_relative_axes(axes1::Axes, axes2::Axes)
-    x_cap_21 = get_relative_direction(get_x_cap(axes1), get_x_cap(axes2))
-    y_cap_21 = rotate_plus_90(x_cap_21)
-    return Axes(x_cap_21, y_cap_21)
-end
-
-function invert_relative_axes(axes::Axes)
-    x_cap_inv = axes |> get_x_cap |> invert_relative_direction
-    y_cap_inv = rotate_plus_90(x_cap_inv)
-    return Axes(x_cap_inv, y_cap_inv)
-end
-
-function invert(pos::SA.SVector{2, T}, axes::Axes{T}) where {T}
-    inv_axes = invert_relative_axes(axes)
-    inv_pos = -rotate(pos, inv_axes)
-    return inv_pos, inv_axes
+function invert(pos::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T}
+    inv_dir = invert_relative_direction(dir)
+    inv_pos = -rotate(pos, inv_dir)
+    return inv_pos, inv_dir
 end
--- src/shapes.jl
+++ src/shapes.jl
@@ -27,9 +27,9 @@ get_tail(line::StdLine{T}, pos::SA.SVector{2, T}) where {T} = pos .+ get_tail(li
 get_vertices(line::StdLine{T}, pos::SA.SVector{2, T}) where {T} = (get_tail(line, pos), get_head(line, pos))
 
 # head, tail, and vertices for StdLine at arbitrary position and orientation
-get_head(line::StdLine{T}, pos::SA.SVector{2, T}, axes::Axes{T}) where {T} = pos .+ (get_half_length(line) * get_x_cap(axes))
-get_tail(line::StdLine{T}, pos::SA.SVector{2, T}, axes::Axes{T}) where {T} = pos .- (get_half_length(line) * get_x_cap(axes))
-get_vertices(line::StdLine{T}, pos::SA.SVector{2, T}, axes::Axes{T}) where {T} = (get_tail(line, pos, axes), get_head(line, pos, axes))
+get_head(line::StdLine{T}, pos::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T} = pos + get_half_length(line) * dir
+get_tail(line::StdLine{T}, pos::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T} = pos - get_half_length(line) * dir
+get_vertices(line::StdLine{T}, pos::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T} = (get_tail(line, pos, dir), get_head(line, pos, dir))
 
 #####
 # StdCircle
@@ -76,19 +76,18 @@ get_top_left(rect::StdRect{T}, pos::SA.SVector{2, T}) where {T} = pos .+ SA.SVec
 get_vertices(rect::StdRect{T}, pos::SA.SVector{2, T}) where {T} = (get_bottom_left(rect, pos), get_bottom_right(rect, pos), get_top_right(rect, pos), get_top_left(rect, pos))
 
 # bottom_left, bottom_right, top_left, top_right, and vertices for StdRect at arbitrary position and orientation
-get_bottom_left(rect::StdRect{T}, pos::SA.SVector{2, T}, axes::Axes{T}) where {T} = pos .- (get_half_width(rect) * get_x_cap(axes)) .- (get_half_height(rect) * get_y_cap(axes))
-get_bottom_right(rect::StdRect{T}, pos::SA.SVector{2, T}, axes::Axes{T}) where {T} = pos .+ (get_half_width(rect) * get_x_cap(axes)) .- (get_half_height(rect) * get_y_cap(axes))
-get_top_right(rect::StdRect{T}, pos::SA.SVector{2, T}, axes::Axes{T}) where {T} = pos .+ (get_half_width(rect) * get_x_cap(axes)) .+ (get_half_height(rect) * get_y_cap(axes))
-get_top_left(rect::StdRect{T}, pos::SA.SVector{2, T}, axes::Axes{T}) where {T} = pos .- (get_half_width(rect) * get_x_cap(axes)) .+ (get_half_height(rect) * get_y_cap(axes))
-
-get_vertices(rect::StdRect{T}, pos::SA.SVector{2, T}, axes::Axes{T}) where {T} = (get_bottom_left(rect, pos, axes), get_bottom_right(rect, pos, axes), get_top_right(rect, pos, axes), get_top_left(rect, pos, axes))
+get_bottom_left(rect::StdRect{T}, pos::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T} = pos - get_half_width(rect) * dir - get_half_height(rect) * rotate_plus_90(dir)
+get_bottom_right(rect::StdRect{T}, pos::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T} = pos + get_half_width(rect) * dir - get_half_height(rect) * rotate_plus_90(dir)
+get_top_right(rect::StdRect{T}, pos::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T} = pos + get_half_width(rect) * dir + get_half_height(rect) * rotate_plus_90(dir)
+get_top_left(rect::StdRect{T}, pos::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T} = pos - get_half_width(rect) * dir + get_half_height(rect) * rotate_plus_90(dir)
+get_vertices(rect::StdRect{T}, pos::SA.SVector{2, T}, dir::SA.SVector{2, T}) where {T} = (get_bottom_left(rect, pos, dir), get_bottom_right(rect, pos, dir), get_top_right(rect, pos, dir), get_top_left(rect, pos, dir))
 
 get_area(rect::StdRect{T}) where {T} = convert(T, 4 * get_half_width(rect) * get_half_height(rect))
 
-get_normals(rect::StdRect{T}) where {T} = get_normals(rect, Axes{T}())
+get_normals(rect::StdRect{T}) where {T} = get_normals(rect, SA.SVector(one(T), zero(T)))
 
-function get_normals(rect::StdRect{T}, axes::Axes{T}) where {T}
-    x_cap = get_x_cap(axes)
-    y_cap = get_y_cap(axes)
+function get_normals(rect::StdRect{T}, dir::SA.SVector{2, T}) where {T}
+    x_cap = dir
+    y_cap = rotate_plus_90(dir)
     return (-y_cap, x_cap, y_cap, -x_cap)
 end
