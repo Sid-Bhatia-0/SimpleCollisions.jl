0ceb1334c6863fb6ee7aa4cbbe25d761602ebd84
Siddharth Bhatia <sidb60200@gmail.com>
2022-01-09 23:59:38 +0530

    rename shapes.jl to standard_shapes.jl

--- src/SimpleCollisions.jl
+++ src/SimpleCollisions.jl
@@ -7,7 +7,7 @@ const Vector2D{T} = SA.SVector{2, T}
 
 include("utils.jl")
 include("position_and_orientation.jl")
-include("shapes.jl")
+include("standard_shapes.jl")
 include("collision_detection.jl")
 include("collision_manifold.jl")
 include("physics.jl")
